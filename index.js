5b14205465bf7fdd1cacfedea5cab85931de74fc
alerrandro buss pickler <alerrandrobusspickler9418@gmail.com>
2026-06-20 16:38:31 -0300

    requisição put feita !

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ app.post('/users', function(req, res){
 
     res.send(newUsers);
 });
+// não modifica o id e nem a idade somente o nome (insonia)
+app.put('/users/:id', function(req, res){
+    const { idade, nome } = req.body;
+
+    const index = usuarios.findIndex(
+        (usuarios) => usuarios.id === parseInt(req.params.id));
+
+    if(index === -1){
+         res.send("Não foi possível encontrar um usuário !");
+    }
+    const editarUsuário = {
+        id: usuarios[index].id,
+        idade,
+        nome,
+    };
+
+    usuarios[index] = editarUsuário;
+
+    res.send(editarUsuário);
+});
 
 //listar os usuarios
 app.get('/users', function(req, res){
@@ -38,6 +58,20 @@ app.get('/users/:id', function(req, res){
     }
      res.send(acharUsuario);
 });
+
+app.delete('/users/:id', function(req, res){
+    const index = usuarios.findIndex(
+        (usuarios) => usuarios.id === parseInt(req.params.id));
+
+    if(index === -1){
+         res.send("Não foi possível encontrar um usuário !");
+    }
+
+    usuarios.splice(0, index);
+    
+     res.send("Usuario excluido com sucesso !");
+});
+
 
 //https://localhost:3000
 app.listen(3000, function(){
