35c3578a1986ac00a347cbb52da6491cff391631
alerrandro buss pickler <alerrandrobusspickler9418@gmail.com>
2026-06-27 18:03:01 -0300

    implementei o graphql com os comandos que presição ser implementados !

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+const { createYoga, createSchema } = require('graphql-yoga');
+const { createServer } = require('node:http');
+
 const usuarios = [
     {
-    id: 1,
-    nome: "Alerrandro",
-    idade: 22
-},
-{
-    id: 2,
-    nome: "Maria",
-    idade: 23
-}
+        id: 1,
+        nome: "Alerrandro",
+        idade: 22
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        idade: 23
+    }
 ];
+
+const typeDefs = `
+    type Usuario {
+        id: Int!
+        nome: String!
+        idade: Int!
+    }
+
+    type Query {
+        usuarios: [Usuario!]!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        usuarios: () => usuarios
+    }
+};
+
+const yoga = createYoga({
+    schema: createSchema({
+        typeDefs,
+        resolvers
+    })
+});
+
+const server = createServer(yoga);
+
+server.listen(4000, () => {
+    console.log("Servidor rodando");
+});
